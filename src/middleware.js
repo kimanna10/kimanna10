23f031a2e276df04c1da78d921f1cc585d3fcfe2
Anna <kimannais1711@gmail.com>
2026-06-10 02:02:42 +0900

    changed: default lang eng

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,12 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 // Обязательно экспортируем результат работы функции как default
-export default createMiddleware(routing);
+// export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  // ВАЖНО: это отключает авто-определение языка по заголовкам браузера
+  localeDetection: false,
+});
 
 // Экспортируем config, чтобы Next.js знал, к каким путям применять мидлвар
 export const config = {
