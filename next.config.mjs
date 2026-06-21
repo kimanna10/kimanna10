445cc3aa53fc37a7e0b699d54866d634abf540e8
Anna <kimannais1711@gmail.com>
2026-06-22 01:30:08 +0900

    edited next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.js");
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dbqsb90lj/**", // Ваш ID облака из ошибки
+      },
+    ],
+  },
 };
 
 // Оборачиваем весь конфиг в плагин
