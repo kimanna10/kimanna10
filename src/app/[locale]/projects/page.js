993710595ec4929f21529284d8999ae0cd48672f
Anna <kimannais1711@gmail.com>
2026-06-21 19:18:02 +0900

    edited revalidate

--- src/app/[locale]/projects/page.js
+++ src/app/[locale]/projects/page.js
@@ -1,6 +1,9 @@
 import ProjectPage from "@/components/ui/ProjectPage"; // Выносим логику клиента сюда
 import { getProjectsFromSheet } from "@/lib/data";
 
+// Эта строка заставляет Vercel обновлять данные каждые 60 секунд
+export const revalidate = 10;
+
 export default async function ProjectsPage() {
   const projects = await getProjectsFromSheet("projects");
 
